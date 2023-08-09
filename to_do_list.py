9825be39970ec168d1d9ede9dffda5ef35aaf0f2
Lucavit9 <95365833+Lucavit9@users.noreply.github.com>
2023-08-09 12:04:55 +0300

    function to do created

--- to_do_list.py
+++ to_do_list.py
@@ -1,2 +1,12 @@
 #create a list to hold to-do tasks
 
+to_do_list = []
+finished = False
+while not finished:
+    task = input("Enter a task for your to-do list.  Press <enter> when done. ")
+
+    if len(task) == 0:
+        finished = True
+    else:
+        to_do_list.append(task)
+        print("task added.")
